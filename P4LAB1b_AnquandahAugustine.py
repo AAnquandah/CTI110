f8d4af3403b644d54e5f40f681a8b4b244b555d0
AAnquandah <129697809+AAnquandah@users.noreply.github.com>
2023-04-27 17:53:59 -0400

    Add files via upload

---
+++ P4LAB1b_AnquandahAugustine.py
@@ -0,0 +1,29 @@
+#CTI 110
+#Anquandah Augustine
+#04/27/2023
+
+
+import turtle
+# A
+t=turtle.Turtle()
+t.penup()
+t.goto(120,50) #centering in the screen
+t.pendown()
+t.pensize(10)
+t.pencolor("red")
+ 
+t.right(65)
+t.forward(100)
+ 
+t.setpos(120,50)
+t.right(50)
+t.forward(100)
+ 
+t.penup()
+t.setpos(100,-10)
+t.right(65)
+t.pendown()
+t.backward(50)
+
+# Hide turtle
+t.hideturtle()
